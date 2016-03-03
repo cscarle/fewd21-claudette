e228870b38424592986530e409fe2136f466c380
cscarle <tempclaud@aol.com>
2016-03-02 22:50:05 -0500

    update 178 changes

---
+++ class04/08_Intro_jQuery/starter_code/color_scheme_switcher/js/index.js
@@ -0,0 +1,30 @@
+jQuery( document ).ready(function() {
+	jQuery('#grayButton').click(switchGray);
+	jQuery('#whiteButton').click(switchWhite);
+
+	function switchGray(){
+		jQuery('body').css("background-color", "gray");
+		console.log("I'm gray!");
+	}
+
+	function switchWhite(){
+		jQuery('body').css("background-color", "white");
+		console.log("I'm white!");
+	}
+
+    // console.log( "ready!" );
+});
+
+
+// document.getElementById('grayButton').onclick = switchGray;
+// document.getElementById('whiteButton').onclick = switchWhite;
+
+// function switchGray() {
+//   document.body.style.backgroundColor = 'gray';
+//  document.body.style.color = 'white';
+// }
+
+// function switchWhite() {
+//   document.body.style.backgroundColor = 'white';
+//   document.body.style.color = 'black';
+// }
