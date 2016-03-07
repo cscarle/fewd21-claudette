ec7f4d294b6bc618c1ab9cee1e692f94cafd8b1e
cscarle <tempclaud@aol.com>
2016-03-07 17:12:02 -0500

    Updates from home

--- class05/08.5_troubleshooting/starter_code/js/exercise.js
+++ class05/08.5_troubleshooting/starter_code/js/exercise.js
@@ -1,11 +1,14 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
 
-    jQuery(".breedContainer .toggler").on("cluck",function(){
+    jQuery(".breedContainer .toggler").on("click",function() {
         jQuery(this).parent().toggleClass("breedContainerLG");
-        if(jQuery(this).attr('src') == "img/240px-Gnome-window-close.svg.png"){
+
+        if(jQuery(this).attr('src') == "img/240px-Gnome-window-close.svg.png") {
             jQuery(this).attr('src',"img/48px-Gnome-list-add.svg.png");
-        }else{
+        }
+        else {
             jQuery(this).attr('src',"img/240px-Gnome-window-close.svg.png");
         }
+
     });
 });
